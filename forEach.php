f58cecc1538463372a804aca9b62d17160b8de29
selvendranks <selvendranks@gmail.com>
2022-09-25 07:56:49 +0530

    added foreach to iterate object

---
+++ forEach.php
@@ -0,0 +1,17 @@
+<?php
+
+$arr = array('donkey','master','goat');
+
+// for($i=0;$i < count($arr);$i++)
+// {
+//   echo($arr[$i]);
+//   echo("<br>");
+// }
+
+foreach($arr as $value){
+    echo "$value <br>";
+}
+
+
+
+?>
